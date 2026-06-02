43f515da0a790b772debbdfa605da7c8ffda554e
alexandremagn2020-ama <alexandremagn2020@gmail.com>
2026-06-02 15:19:58 -0300

    Update app.py

--- .devcontainer/app.py
+++ .devcontainer/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title="Portal de Performance")
 
 st.markdown("<h2 style='text-align: center; color: #1E3A8A; font-weight: 700;'>🎯 PORTAL DE CAMPANHAS DE VENDAS</h2>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color: #6B7280; text-align: center;'>Bem-vindo ao sistema de apuração corporativo.</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: #6B7280;'>Bem-vindo ao sistema de apuração corporativo.</p>", unsafe_allow_html=True)
 st.write("---")
 
-st.info("💡 Uma barra lateral de navegação foi criada automaticamente pelo sistema! Utilize o menu ao lado esquerdo da sua tela para escolher entre visualizar os dados consolidados do **1º Quadrimestre** ou o mês isolado de **Maio/2026**.")
+st.info("💡 Use o menu lateral para navegar entre o **1º Quadrimestre** e **Maio/2026**.")
