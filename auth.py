eeeb2ccc383584348594d2c8f320e43054b4575d
alexandremagn2020-ama <alexandremagn2020@gmail.com>
2026-06-02 16:40:23 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-SENHA_CORRETA = "bacon"
-
 def validar_senha():
-    senha = st.session_state.get("senha", "")
-    if senha != SENHA_CORRETA:
+    if not st.session_state.get("autenticado", False):
         st.error("Acesso negado. Volte à página inicial e digite a senha correta.")
         st.stop()
